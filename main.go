439af542a9fd3258b94a0b0449b4d08177cc586f
Danillo Barreto <danillosbarreto@gmail.com>
2018-08-03 16:19:44 -0300

    Mudando simulação de post de referenceloader para stringloader

--- main.go
+++ main.go
@@ -9,9 +9,25 @@ import (
 func main() {
 	pwd, _ := os.Getwd()
 	schemaLoader := gojsonschema.NewReferenceLoader("file:///" + pwd + "/schema/schema.json")
-	documentLoader := gojsonschema.NewReferenceLoader("file:///" + pwd + "/schema/post.json")
 
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	post := `{
+		"title": "Desenvolvedor Gopher",
+		"role": {
+			"id" : 446
+		},
+		"cities": [{
+			"id": 1,
+			"quantity": 2
+		},{
+			"id": 3,
+			"quantity": 10
+		}],
+		"salaryNegotiable": false
+	}`
+
+	stringLoader := gojsonschema.NewStringLoader(post)
+
+	result, err := gojsonschema.Validate(schemaLoader, stringLoader)
     if err != nil {
         panic(err.Error())
     }
